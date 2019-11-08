1879810722a5efac1ec71160cbf9ce92496f8b4c
Justin Brandley <brandleyj@gmail.com>
2019-11-08 11:53:30 -0600

    added routes to server.js

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const helmet = require("helmet");
 
-const recipeRouter = require("./router/");
+const projectRouter = require("./router/projects/projects-router");
+const taskRouter = require("./router/tasks/tasks-router");
+const resourceRouter = require("./router/resources/resources-router");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use("/api/", recipeRouter);
+server.use("/api/projects", projectRouter);
+server.use("/api/tasks", taskRouter);
+server.use("/api/resources", resourceRouter);
 
 server.get("/", (req, res) => {
 	return res.send("<h1>Recipe Book</h1>");
